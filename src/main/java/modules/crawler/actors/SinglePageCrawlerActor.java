bb0744df39bab7e5b12b5206ca82fe7cd79d8f3d
Grzegorz Olechwierowicz <golechwierowicz@gmail.com>
2017-11-01 11:22:29 +0100

    Fix config for local dev and stop singlepage crawler actor after it crawled its website

--- src/main/java/modules/crawler/actors/SinglePageCrawlerActor.java
+++ src/main/java/modules/crawler/actors/SinglePageCrawlerActor.java
@@ -79,6 +79,7 @@ public class SinglePageCrawlerActor extends AbstractActor {
                                 } else slave.tell(crNew, getSelf());
                             }
                         }
+                        getContext().getSystem().stop(self());
                     }
                 })
                 .matchAny(any -> log.warning("Received unknown message...{}", any))
